ff272bd72fb1be795c5e2c4f6ceeecf7a03694ea
Celticcow <gregory.dunlap@gmail.com>
2020-08-13 13:08:00 -0500

    added web and index var

--- groupsearch.py
+++ groupsearch.py
@@ -14,6 +14,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 """
 Greg_Dunlap / CelticCow
+get list of domains, search contents of group
 """
 
 """
@@ -22,7 +23,7 @@ get list of domains
 def get_domains(ip_addr):
     domain_list = []
     end = "\n"
-    debug = 1
+    debug = 0
 
     try:
         domain_sid = apifunctions.login('roapi', '1qazxsw2', ip_addr, "")
@@ -35,7 +36,7 @@ def get_domains(ip_addr):
             print(json.dumps(get_domain_result), end=end)
         
         for x in range(get_domain_result['total']):
-            print(get_domain_result['objects'][x]['name'], end=end)
+            #print(get_domain_result['objects'][x]['name'], end=end)
 
             domain_list.append(get_domain_result['objects'][x]['name'])
         
@@ -53,9 +54,13 @@ search cma for a group with name group_name
 recur is to call function recursivly looking for a group in a group
 sid can be passed in as a sid if we're calling this recursive
 """
-def search_cma(mds_ip, cma_ip, group_name, recur = 0, sid=""):
-    debug = 1
+def search_cma(mds_ip, cma_ip, group_name, recur = 0, sid = ""):
+    debug = 0
     end = "\n"
+
+    marker = ""
+    for m in range(recur):
+        marker = marker + "\t"
 
     try:
         if(sid == ""):
@@ -74,7 +79,7 @@ def search_cma(mds_ip, cma_ip, group_name, recur = 0, sid=""):
         else:
             for x in range(chkname['total']):
                 if(chkname['objects'][x]['type'] == "group"):
-                    print(chkname['objects'][x]['name'], end=end)
+                    print(marker + chkname['objects'][x]['name'], end=end)
 
                     get_group_data = {"name" : group_name}
                     group_contents = apifunctions.api_call(mds_ip, "show-group", get_group_data, cma_sid)
@@ -85,25 +90,28 @@ def search_cma(mds_ip, cma_ip, group_name, recur = 0, sid=""):
                         print("---------------------------------", end=end)
 
                     group_len = len(group_contents['members'])
+                    print(marker + "Number of items in group", end=" ")
                     print(group_len, end=end)
 
                     for y in range(group_len):
-                        print("Name : " + group_contents['members'][y]['name'], end=" - ")
+                        print(marker + "Name : " + group_contents['members'][y]['name'], end=" - ")
                         print("Type : " + group_contents['members'][y]['type'], end=end)
                         if(group_contents['members'][y]['type'] == "network"):
-                            print(group_contents['members'][y]['subnet4'], end=" / ")
+                            print(marker + group_contents['members'][y]['subnet4'], end=" / ")
                             print(group_contents['members'][y]['mask-length4'], end=end)
                         elif(group_contents['members'][y]['type'] == "host"):
-                            print(group_contents['members'][y]['ipv4-address'], end=end)
+                            print(marker + group_contents['members'][y]['ipv4-address'], end=end)
                         elif(group_contents['members'][y]['type'] == "address-range"):
-                            print(group_contents['members'][y]['ipv4-address-first'], end=" - ")
-                            print(group_contents['members'][y]['ipv4-address-last'], end=end)
+                            print(marker + group_contents['members'][y]['ipv4-address-first'], end=" - ")
+                            print(marker + group_contents['members'][y]['ipv4-address-last'], end=end)
                         elif(group_contents['members'][y]['type'] == "group"):
-                            print("WARNING, searching group inside of group", end=end)
-                            search_cma(mds_ip, cma_ip, group_contents['members'][y]['name'], 1, cma_sid)
+                            print(marker + "Traversing into sub group ", end=end)
+                            search_cma(mds_ip, cma_ip, group_contents['members'][y]['name'], recur + 1, cma_sid)
                         else:
-                            print("unknown type for output", end=end)
-                            print("send search param to greg@fedex.com", end=end)
+                            print(marker + "unknown type for output", end=end)
+                            print(marker + "send search param to greg@fedex.com", end=end)
+                        
+                        print("--------------", end=end)
                     #end of for y in range
         #end of else
         if(recur == 1):
@@ -124,9 +132,11 @@ def main():
     mds_ip = "146.18.96.16"
     domain_list = get_domains(mds_ip)
 
+    grp_to_find = "hublab-subnets"
+
     for x in domain_list:
-        print(x, end=end)
-        search_cma(mds_ip, x, "hublab-subnets")
+        print("Searching CMA : " + x, end=end)
+        search_cma(mds_ip, x, grp_to_find)
 #end of main
 
 if __name__ == "__main__":
